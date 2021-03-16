331504f54c86cb7052ac715a0a92f35e0b7d0b61
IgorFilimonov <st087840@student.spbu.ru>
2021-03-16 22:42:11 +0300

    added solution

---
+++ src/main/kotlin/homework1/task1.kt
@@ -0,0 +1,36 @@
+package homework1
+
+import kotlin.system.exitProcess
+
+fun readNumber() : Int
+{
+    println("Enter an integer non-negative number:")
+    val number = readLine()?.toIntOrNull()
+    if (number == null || number < 0) {
+        println("It's not a non-negative integer")
+        exitProcess(1)
+    }
+    return number
+}
+
+fun computeFactorialRecursive(number: Int) : Int
+{
+    if (number == 0)
+        return 1;
+    return computeFactorialRecursive(number - 1) * number
+}
+
+fun computeFactorialIterative(number: Int) : Int
+{
+    var factorial = 1
+    for (i in 2..number)
+        factorial *= i
+    return factorial
+}
+
+fun main()
+{
+    val number = readNumber()
+    println("FactorialRecursive: ${computeFactorialRecursive(number)}")
+    println("FactorialIterative: ${computeFactorialIterative(number)}")
+}
