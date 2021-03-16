9619b03d66cdc91c208630bfc668aa8b80390309
IgorFilimonov <st087840@student.spbu.ru>
2021-03-17 00:47:05 +0300

    trying to fix the code style

--- src/main/kotlin/homework1/task1.kt
+++ src/main/kotlin/homework1/task1.kt
@@ -2,34 +2,30 @@ package homework1
 
 import kotlin.system.exitProcess
 
-fun readNumber() : Int
-{
+fun readNumber() : Int {
     println("Enter an integer non-negative number:")
     val number = readLine()?.toIntOrNull()
     if (number == null || number < 0) {
         println("It's not a non-negative integer")
-        exitProcess(-1)
+        exitProcess(1)
     }
     return number
 }
 
-fun computeFactorialRecursive(number: Int) : Int
-{
+fun computeFactorialRecursive(number: Int) : Int {
     if (number == 0)
         return 1
     return computeFactorialRecursive(number - 1) * number
 }
 
-fun computeFactorialIterative(number: Int) : Int
-{
+fun computeFactorialIterative(number: Int) : Int {
     var factorial = 1
     for (i in 2..number)
         factorial *= i
     return factorial
 }
 
-fun main()
-{
+fun main() {
     val number = readNumber()
     println("FactorialRecursive: ${computeFactorialRecursive(number)}")
     println("FactorialIterative: ${computeFactorialIterative(number)}")
