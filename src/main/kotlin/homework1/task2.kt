74302bc01a14f8b4a57981f00f25f39601b58dbb
IgorFilimonov <st087840@student.spbu.ru>
2021-03-17 13:01:51 +0300

    added solution

---
+++ src/main/kotlin/homework1/task2.kt
@@ -0,0 +1,21 @@
+package homework1
+
+fun findOccurrences(string1: String, string2: String): Int {
+    var numberOfOccurrences = 0
+    for (i in string1.indices) {
+        for (j in string2.indices) {
+            if (i + j >= string1.length || string1[i + j] != string2[j])
+                break
+            if (j + 1 == string2.length)
+                ++numberOfOccurrences
+        }
+    }
+    return numberOfOccurrences
+}
+
+fun main() {
+    println("Enter each string on a separate line:")
+    val string1 = readLine() ?: error("It's not a string")
+    val string2 = readLine() ?: error("It's not a string")
+    println("Number of occurrences: ${findOccurrences(string1, string2)}")
+}
