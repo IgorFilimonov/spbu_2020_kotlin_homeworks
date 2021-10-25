6f725715f7cc47570303e7e657007061a7f50ffe
IgorFilimonov <st087840@student.spbu.ru>
2021-10-25 23:09:54 +0300

    .

--- src/main/kotlin/test2/task1.kt
+++ src/main/kotlin/test2/task1.kt
@@ -1,9 +1,9 @@
+@file: Suppress("MatchingDeclarationName")
 package test2
 
 import tornadofx.App
 import tornadofx.launch
 
-@Suppress("MatchingDeclarationName")
 class BrowserApp : App(StartView::class)
 
 fun main() {
